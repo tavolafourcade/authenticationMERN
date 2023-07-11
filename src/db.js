f563131d828a93a7a6285fdb5dcc528afe20a469
tavolafourcade <tavolafourcade@gmail.com>
2023-07-10 22:13:48 -0500

    [ DELETE ] Commented code

--- src/db.js
+++ src/db.js
@@ -3,14 +3,6 @@ import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-// mongoose.connect(process.env.MONGO_URL,
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(db => console.log('Db connected'))
-//   .catch(error => console.log(error))
-
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL,
