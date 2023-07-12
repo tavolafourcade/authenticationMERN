558bd9fd7a3c6ecaaa7c82cf6d52dc45d79d4e16
tavolafourcade <tavolafourcade@gmail.com>
2023-07-11 19:56:17 -0500

    [ ADD ] Hashing password [ UPDATE ] Response after register a new user

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,16 +1,25 @@
 import User from '../models/user.model.js'
+import bcrypt from 'bcryptjs'
 
 export const register = async (req, res) => {
   const { email, password, username } = (req.body)
 
   try {
+
+    const passwordHash = await bcrypt.hash(password, 10)
     const newUser = new User({
       email,
-      password,
+      password: passwordHash,
       username
     })
     const userSaved = await newUser.save()
-    res.json(userSaved)
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt
+    })
   } catch (error) {
     console.log(error)
   }
