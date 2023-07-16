39818eb1694f0c3256900a0770c4a3be63f15d03
tavolafourcade <tavolafourcade@gmail.com>
2023-07-15 20:41:58 -0500

    [ ADD ] Login creation

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -31,4 +31,31 @@ export const register = async (req, res) => {
     res.status(500).json({ message: error.message })
   }
 }
-export const login = (req, res) => res.send('login')
+
+export const login = async (req, res) => {
+  const { email, password } = (req.body)
+
+  try {
+    const userFound = await User.findOne({email})
+    if (!userFound) return res.status(400).json({ message: 'User not found' })
+
+    // Compare password from request with password from database
+    const isMatch = await bcrypt.compare(password, userFound.password)
+
+    if (!isMatch) return res.status(400).json({ message: 'Incorrect password' })
+
+    // Create token from user id
+    const token = await createAccessToken({id: userFound._id})
+    res.cookie('token', token) // Set-Cookie: token=token
+
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      createdAt: userFound.createdAt,
+      updatedAt: userFound.updatedAt
+    })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
