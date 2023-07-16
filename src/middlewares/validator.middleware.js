bcf153e0b82c3685bcbc4319b5455667374d31b7
tavolafourcade <tavolafourcade@gmail.com>
2023-07-16 10:37:10 -0500

    [ ADD ] Creation of Schema to validate Register and Login

---
+++ src/middlewares/validator.middleware.js
@@ -0,0 +1,8 @@
+export const validateSchema = (schema) => (req, res, next) => {
+  try {
+    schema.parse(req.body)
+    next()
+  } catch (error) {
+    res.status(400).json({ error: error.errors.map((error) => error.message) })
+  }
+}
