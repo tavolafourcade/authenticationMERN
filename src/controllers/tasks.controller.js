ad514e9d284aec85141240559b2c99010c66ed06
tavolafourcade <tavolafourcade@gmail.com>
2023-07-16 01:44:56 -0500

    [ ADD ] Creation of tasks CRUD

---
+++ src/controllers/tasks.controller.js
@@ -0,0 +1,40 @@
+import Task from '../models/task.model.js'
+
+export const getTasks = async (req, res) => {
+  const tasks = await Task.find({
+    user: req.user.id
+  }).populate('user')
+  res.json(tasks)
+}
+
+export const createTask = async (req, res) => {
+  const { title, description, date } = req.body
+  const newTask = new Task({
+    title,
+    description,
+    date,
+    user: req.user.id
+  })
+  const savedTask = await newTask.save()
+  res.json(savedTask)
+}
+export const getTask = async (req, res) => {
+  const task = await Task.findById(req.params.id).populate('user')
+  if (!task) return res.status(404).json({ message: 'Task not found' })
+  res.json(task)
+}
+export const deleteTask = async (req, res) => {
+  const task = await Task.findByIdAndDelete(req.params.id)
+
+  // task will be the task deleted
+  if (!task) return res.status(404).json({ message: 'Task not found' })
+  return res.sendStatus(204)
+}
+export const updateTask = async (req, res) => {
+  // By default mongoose return the previous task so we set new: true
+  const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+  // task will be the task updated
+  if (!task) return res.status(404).json({ message: 'Task not found' })
+  res.json(task)
+}
